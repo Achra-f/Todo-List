c50977c791e87cc35cf6da5eadef71a73dcfaaee
Achraf <91100199+Achra-f@users.noreply.github.com>
2023-06-24 18:12:18 +0200

    added html/css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './style.css';
+
+const openModalButtons = document.querySelectorAll('.open-modal');
+const closeModalButtons = document.querySelectorAll('.close-modal');
+const modalOverlay = document.querySelector('.modal-overlay');
+
+openModalButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const modalId = button.dataset.modal;
+    const modal = document.getElementById(modalId);
+    modalOverlay.style.display = 'flex';
+    modal.style.display = 'block';
+  });
+});
+
+closeModalButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    modalOverlay.style.display = 'none';
+    button.parentElement.style.display = 'none';
+  });
+});
