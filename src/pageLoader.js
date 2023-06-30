162d6f4ff4be03420dc28005e0259c2ad3db61c3
Achraf <91100199+Achra-f@users.noreply.github.com>
2023-06-30 22:33:00 +0200

    display projects and tasks

--- src/pageLoader.js
+++ src/pageLoader.js
@@ -18,15 +18,41 @@ export class PageLoader {
         projects.forEach((project) => {
             const projectElement = document.createElement('div');
             projectElement.textContent = project;
+            projectElement.classList.add('sidebar__project');
             this.projectsElement.appendChild(projectElement);
         });
+
+        const addProjectButton = document.createElement('button');
+        addProjectButton.textContent = 'Add Project';
+        addProjectButton.classList.add('sidebar__project');
+        this.projectsElement.appendChild(addProjectButton);
+
+
     }
 
     renderTasks(tasks) {
+        const tasksEl = document.createElement('div');
+        tasksEl.classList.add('tasks');
+        tasksEl.textContent = 'Inbox';
+
+        const taskList = document.createElement('div');
+        taskList.classList.add('main-content__todo');
+
         tasks.forEach((task) => {
             const taskElement = document.createElement('div');
             taskElement.textContent = task;
-            this.contentElement.appendChild(taskElement);
+            taskElement.classList.add('main-content__todos');
+            taskList.appendChild(taskElement);
         });
+
+        const addTaskButton = document.createElement('button');
+        addTaskButton.textContent = 'Add New Task';
+        addTaskButton.classList.add('main-content__todos');
+        addTaskButton.setAttribute('data-modal', 'task');
+        taskList.appendChild(addTaskButton);
+
+        this.contentElement.appendChild(tasksEl);
+        this.contentElement.appendChild(taskList);
     }
+
 }
