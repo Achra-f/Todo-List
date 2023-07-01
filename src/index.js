73824f78510a2beb917d124b08028077c6c2d3ef
Achraf <91100199+Achra-f@users.noreply.github.com>
2023-07-01 23:03:13 +0200

    display projects and tasks

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import './style.css';
+import { Project } from './project';
+import { Task } from './task';
+import { PageLoader } from './pageLoader';
+
+
+const pageLoader = new PageLoader();
+
+const inbox = new Project('Inbox');
+inbox.addTask(new Task('Inbox Task 1', 'Description 1', '2023-07-05', 'High', 'Some notes', ['Task 1 Subtask 1', 'Task 1 Subtask 2']));
+
+const project1 = new Project('Project 1');
+project1.addTask(new Task('Project 1 Task 1', 'Description 1', '2023-07-10', 'Medium', 'Some notes', []));
+
+const project2 = new Project('Project 2');
+project2.addTask(new Task('Project 2 Task 1', 'Description 1', '2023-07-15', 'Low', 'Some notes', []));
+
+const project3 = new Project('Project 3');
+project3.addTask(new Task('Project 3 Task 1', 'Description 1', '2023-07-20', 'High', 'Some notes', ['Task 1 Subtask 1', 'Task 1 Subtask 2', 'Task 1 Subtask 3']));
+
+const projects = [inbox, project1, project2, project3];
+
+pageLoader.loadProjects(projects);
+
+
+
---
+++ src/project.js
@@ -0,0 +1,21 @@
+export class Project {
+    constructor(title) {
+        this.title = title;
+        this.tasks = [];
+    }
+
+    addTask(task) {
+        this.tasks.push(task);
+    }
+
+    removeTask(task) {
+        const index = this.tasks.indexOf(task);
+        if (index > -1) {
+            this.tasks.splice(index, 1);
+        }
+    }
+
+    getTitle() {
+        return this.title;
+    }
+}
